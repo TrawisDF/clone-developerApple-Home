bfbd0d3717ee05fcc7128ca4349b1a9012678eec
TrawisDF <134593322+TrawisDF@users.noreply.github.com>
2024-02-13 17:07:00 +0700

    today is enough!

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,13 +1,27 @@
+"use client";
+
+import { useState } from "react";
+
 const Navbar = () => {
+  const [hambergToggle, setHambergToggle] = useState(false);
+  const [hambergMenu, setHambergMenu] = useState("");
+  const hambergHandler = () => {
+    setHambergToggle(!hambergToggle);
+    if (hambergToggle) {
+      setHambergMenu("block");
+    } else {
+      setHambergMenu("hidden");
+    }
+  };
   return (
-    <div className="pd-5 bg-[#e3e1e1ce] min-w-1024px max-w-[100%] mx-auto sticky top-0 z-20 backdrop-filter backdrop-blur-sm">
-      <ul className="flex justify-between max-w-[980px] mx-auto items-center">
+    <div className="pd-5 bg-[#e3e1e1] min-w-1024px max-w-[100%] mx-auto sticky top-0 z-20 backdrop-filter backdrop-blur-sm">
+      <ul className="justify-between max-w-[980px] mx-auto items-center hidden lg:flex">
         <li>
           <a href="#">
             <img
               style={{ width: "135px", height: "44px" }}
               src="https://developer.apple.com/assets/elements/icons/brandmark/apple-developer-brandmark.svg"
-            ></img>
+            />
           </a>
         </li>
         <li>
@@ -23,7 +37,7 @@ const Navbar = () => {
           <a href="#develop">Develop</a>
         </li>
         <li>
-          <a href="#support">Suppot</a>
+          <a href="#support">Support</a>
         </li>
         <li>
           <a href="#account">Account</a>
@@ -37,6 +51,82 @@ const Navbar = () => {
           </a>
         </li>
       </ul>
+      <div className="flex justify-between mx-10 lg:hidden">
+        <button
+          onClick={() => {
+            hambergHandler();
+          }}
+          className="lg:hidden text-[#7e7e7e]"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-6 h-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
+        </button>
+        <div>
+          <img
+            style={{ width: "135px", height: "44px" }}
+            src="https://developer.apple.com/assets/elements/icons/brandmark/apple-developer-brandmark.svg"
+          />
+        </div>
+        <div></div>
+      </div>
+      <div className={`lg:hidden ${hambergMenu} w-[980px]`}>
+        <ul className="p-4 bg-[#f4f0f0] z-20 absolute inset-x-0 }">
+          <li>
+            <div className="border-b-2 border-[#817c7c] pb-2 flex justify-center">
+              <div className="w-full">
+                <input type="text" placeholder="Search" className="w-full mx-2 rounded-md bg-[#d7d7d7]"/>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div className="p-2 flex justify-start mx-2 border-b-2">
+              <a href="#news">News</a>
+            </div>
+          </li>
+
+          <li>
+            <div className="p-2 flex justify-start mx-2 border-b-2">
+              <a href="#sidcover">Discover</a>
+            </div>
+          </li>
+
+          <li>
+            <div className="p-2 flex justify-start mx-2 border-b-2">
+              <a href="#design">Design</a>
+            </div>
+          </li>
+
+          <li>
+            <div className="p-2 flex justify-start mx-2 border-b-2">
+              <a href="#develop">Develop</a>
+            </div>
+          </li>
+
+          <li>
+            <div className="p-2 flex justify-start mx-2 border-b-2">
+              <a href="#support">Support</a>
+            </div>
+          </li>
+
+          <li>
+            <div className="p-2 flex justify-start mx-2 border-b-2">
+              <a href="#account">Account</a>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
